47c012f4b71d465888da8b8040b28ac616f4a8ff
程亮然 <1519790385@qq.com>
2017-09-27 17:58:34 +0800

    111

---
+++ src/main/java/com/clr/common/JSONMap.java
@@ -0,0 +1,13 @@
+package com.clr.common;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+/**
+ * Created by Administrator on 2017/9/27 0027.
+ */
+public class JSONMap {
+    Map<String ,Object> jsonMap=new HashMap<String,Object>();
+
+}
